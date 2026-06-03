75fca52d8661da05e390f085aa64538747f81d84
Suporte Accesys <131163510+suporteaccesys@users.noreply.github.com>
2026-06-03 09:19:54 -0300

    Update install-rustdesk.bat

--- RustDesk/install-rustdesk.bat
+++ RustDesk/install-rustdesk.bat
@@ -1,1 +1,5 @@
+Primeiro: 
 iwr -useb http://45.231.133.254:8080/InstalarRustDesk.bat -OutFile "$env:TEMP\i.bat"; Start-Process "$env:TEMP\i.bat"
+
+Se caso ele nao configurar a senha rode esse comando:
+iwr -useb http://45.231.133.254:8080/brand.ps1 | iex
